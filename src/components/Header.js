470b7f6b015478a17b4f487ff10e0caccd1d053b
mi115102 <michael.setzer@ucf.edu>
2024-03-13 17:02:40 -0400

    39

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
@@ -13,7 +19,7 @@ Header.defaultProps = {
 }
 
 Header.propTypes = {
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
 }
 
 // CSS in JS
